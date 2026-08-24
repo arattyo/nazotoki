b7443fecf0ad334c354fba766de8a29f1305d4f1
arattyo <arasahirot@open.ed.jp>
2026-08-24 11:27:09 +0900

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,63 @@
+const questions = [
+
+{
+    id: 1,
+    title: "第1問",
+    image: "images/q1.png",
+    question: "",
+    hint: "左側は「はく？」、右側は「？がら」と読めそうです。",
+    answers: [
+        "昼間",
+        "ひるま"
+    ]
+},
+
+{
+    id: 2,
+    title: "第2問",
+    image: "images/q2.png",
+    question: "",
+    hint: "5つの四角には4段とも同じ文字が入ります。2段目の白い部分を読むと「進化」になるので、1段目は「鹿」になりそうですね。",
+    answers: [
+        "瞬間",
+        "しゅんかん"
+    ]
+},
+
+{
+    id: 3,
+    title: "第3問",
+    image: "images/q3.png",
+    question: "",
+    hint: "化学が関連してきそうですね。",
+    answers: [
+        "守備",
+        "しゅび"
+    ]
+},
+
+{
+    id: 4,
+    title: "第4問",
+    image: "images/q4.png",
+    question: "",
+    hint: "ローマ字に変更してみると良さそうです。",
+    answers: [
+        "表",
+        "おもて"
+    ]
+},
+
+{
+    id: 5,
+    title: "第5問",
+    image: "images/q5.png",
+    question: "",
+    hint: "これまでの問題を振り返るとアルファベットが反転していますね。答えに注目してみましょう。",
+    answers: [
+        "八重高",
+        "やえこう"
+    ]
+}
+
+];
